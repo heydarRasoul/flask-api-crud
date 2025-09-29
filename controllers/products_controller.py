aa7830e93c015dadc2b6da3bf358ddebab912f1f
Heydar Rasoulpouranaei <heydarrasoulpour@ddevpipeline.com>
2025-09-29 16:48:07 -0600

    Update base on feedback

--- controllers/products_controller.py
+++ controllers/products_controller.py
@@ -6,7 +6,7 @@ def create_product():
     data = request.form if request.form else request.get_json()
 
     if not data:
-        return({"error": "No input provided"}), 400
+        return({"error": "no input provided"}), 400
 
     try:
         product ={}
@@ -16,29 +16,35 @@ def create_product():
         product['price'] = float(data['price'])
         product['active'] = bool(data['active'])
         product_records.append(product)
-        return({"message": f"Product {product['product_name']} has been added."}), 200
+        return({"message": f"product created", "result":product}), 201
 
     except (error) as e :
-        return({"error": f" Invalid input: {e}"}), 400
+        return({"error": f" invalid input: {e}"}), 400
 
 
 def read_all_products():
     if not product_records:
-        return jsonify({"message": "No products found"}), 404
+        return jsonify({"message": "no products found"}), 404
     return jsonify(product_records), 200
 
 def read_product_by_id(product_id):
     for product in product_records:
         if product['product_id'] == int(product_id):
             return jsonify (product), 200
-    return jsonify({"message": f'Product {product["product_id"]} not found.'}), 404
+    return jsonify({"message": f'product not found.'}), 404
 
-    
+
 def read_active_products():
+    active_products = []
+
     for product in product_records:
         if product.get("active"):
-            return jsonify (product), 200
-    return jsonify({"message": "No active product found."}), 404
+            active_products.append(product)
+
+    if active_products:
+        return jsonify(active_products), 200
+    else:
+        return jsonify({"message": "no active product found."}), 404
 
 
 def update_product_by_id(product_id):
@@ -47,7 +53,7 @@ def update_product_by_id(product_id):
 
     for product in product_records:
         if product.get ('product_id') == product_id:
-            if "product name" in data:
+            if "product_name" in data:
                 product['product_name'] = data['product_name']
             if "description" in data:
                 product['description'] = data['description']
@@ -56,10 +62,10 @@ def update_product_by_id(product_id):
             if "active" in data:
                 product['active'] = data['active']
             return jsonify({
-                "message": f"Product {product_id} updated successfully.",
+                "message": f"product updated successfully.",
                 "product": product
             }), 200
-    return jsonify({"message": f"Product {product_id} not found."}), 404           
+    return jsonify({"message": f"product not found."}), 404           
 
 
 def delete_product_by_id(product_id):
@@ -68,5 +74,5 @@ def delete_product_by_id(product_id):
     for index, product in enumerate(product_records):
         if product['product_id'] == product_id:
             deleted_product = product_records.pop(index)
-        return jsonify({"message":"Product {product_id} has been removed."}), 200
-    return jsonify({"message": f"Product {product_id} not found."}), 404
+        return jsonify({"message":"product has been removed."}), 200
+    return jsonify({"message": f"product not found."}), 404
